dd777fbc725a7a514903484cc3f1d60ce2d63514
Maciej Dowgird <dowgird.maciej@gmail.com>
2020-10-14 17:39:41 +0200

    Test data from "titanic" dataset loaded. .gitignore updated.

--- create_model/data_transformer.py
+++ create_model/data_transformer.py
@@ -1,7 +1,11 @@
-
+from sklearn.pipeline import make_pipeline
 
 class Transformer:
     """Wrapper for pipeline for transformations of input and output data."""
 
-    def __init__(self):
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+    def transform(self):
         pass
--- test_model.py
+++ test_model.py
@@ -1,19 +1,64 @@
 from create_model.model_finder import ModelFinder
 
-from sklearn.datasets import load_digits
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
-
 from sklearn.metrics import accuracy_score
 
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer
 
+import os
+import pandas as pd
+import numpy as np
 
 if __name__ == "__main__":
-    d = load_digits(as_frame=True)
 
-    X = d["data"]
-    y = d["target"]
+    data_directory = os.path.join(os.getcwd(), "data", "titanic")
+    output_directory = os.path.join(os.getcwd(), "output", "titanic")
+    train_file = os.path.join(data_directory, "train.csv")
+    test_file = os.path.join(data_directory, "test.csv")
+
+    train_df = pd.read_csv(train_file)
+    test_df = pd.read_csv(test_file)
+
+    target = "Survived"
+
+    features = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Embarked"]
+
+    X = train_df[features]
+    y = train_df[target]
+
+    imp = SimpleImputer(strategy="most_frequent")
+    imp.fit(X)
+    X = pd.DataFrame(imp.transform(X), columns=X.columns)
+
+    X_test = test_df[features]
+    X_test = pd.DataFrame(imp.transform(X_test), columns=X_test.columns)
+
+    def transform_df(dataframe):
+        dataframe["SibSp_bool"] = np.where(dataframe["SibSp"] != 0, 1, 0)
+        dataframe["Parch_bool"] = np.where(dataframe["Parch"] != 0, 1, 0)
+
+        gender_map = {"female": 1, "male": 0}
+        dataframe["Sex_bool"] = dataframe["Sex"].map(gender_map)
+
+        dataframe = dataframe.drop(["SibSp", "Parch", "Sex"], axis=1)
+
+        return dataframe
+
+    X = transform_df(X)
+    X_test = transform_df(X_test)
+
+    # fitting only to train_df
+    enc = OneHotEncoder(handle_unknown="ignore")
+    enc.fit(X["Embarked"].to_numpy().reshape(-1, 1))
+
+    X = pd.concat([X, pd.DataFrame(enc.transform(X["Embarked"].to_numpy().reshape(-1, 1)).toarray())], axis=1)
+    X_test = pd.concat([X_test, pd.DataFrame(enc.transform(X_test["Embarked"].to_numpy().reshape(-1, 1)).toarray())], axis=1)
+
+    X = X.drop(["Embarked"], axis=1)
+    X_test = X_test.drop(["Embarked"], axis=1)
 
     model_rfc = RandomForestClassifier
     param_grid_rfc = {
@@ -50,3 +95,10 @@ if __name__ == "__main__":
     model, score, params = finder.find_best_model()
 
     print("Model: {}\nScore: {}\nParams: {}".format(model.__name__, score, params))
+
+    clf = model(**params)
+    clf.fit(X, y)
+    # predictions from test dataset
+    predictions = clf.predict(X_test)
+    output = pd.DataFrame({'PassengerId': test_df["PassengerId"], 'Survived': predictions})
+    output.to_csv(os.path.join(output_directory, "submission.csv"), index=False)
